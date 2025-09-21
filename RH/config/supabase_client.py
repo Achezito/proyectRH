6de20f05b872435fd914ff3d1f50813a59f8cc40
Achezito <achezzito@gmail.com>
2025-09-20 22:23:28 -0700

    Creacionn de expo y rutas

--- RH/config/supabase_client.py
+++ RH/config/supabase_client.py
@@ -1,10 +1,6 @@
-from flask import Flask, request, jsonify
-
-from supabase import create_client, Client
 import os
+from supabase import create_client, Client
 
-
-supabase_url = os.environ.get("SUPABASE_URL")
-supabase_key = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsdG51YmZqdnlwcmNkdWpoa3FpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyNDc1NDgsImV4cCI6MjA3MzgyMzU0OH0.n5j8uIghjwQiYDyzo7cpSakyMZn22QevjXK7nvV5UR0")
-
-supabase: Client = create_client(supabase_url, supabase_key)
+url: str = "https://iltnubfjvyprcdujhkqi.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsdG51YmZqdnlwcmNkdWpoa3FpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyNDc1NDgsImV4cCI6MjA3MzgyMzU0OH0.n5j8uIghjwQiYDyzo7cpSakyMZn22QevjXK7nvV5UR0"
+supabase: Client = create_client(url, key)
