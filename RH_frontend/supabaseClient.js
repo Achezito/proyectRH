8dd9e8770bd72689e33bafbcbb7b94262f2fa022
Achezito <achezzini@hotmail.com>
2025-12-08 03:25:53 -0800

    cambios

--- RH_frontend/supabaseClient.js
+++ RH_frontend/supabaseClient.js
@@ -1,8 +1,9 @@
-// supabaseClient.js
+// supabaseClient.js - PARA TESTING
 import { createClient } from "@supabase/supabase-js";
-import { supabaseConfig } from "./src/config/api";
 
-export const supabase = createClient(
-  supabaseConfig.url,
-  supabaseConfig.anonKey
-);
+// URLs de ejemplo (REMPLAZA CON LAS TUS)
+const SUPABASE_URL = "https://rtwcoftbxtqnpheakuuu.supabase.co";
+const SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0d2NvZnRieHRxbnBoZWFrdXV1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MjU0NTkyMywiZXhwIjoyMDY4MTIxOTIzfQ.4qNCzAjJ99W6tFz-YlLAWp7ZD9yAgPBFrpDix3D-C34";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
